c09aa70ff3b3f8fa5c39264d143494538cdf9aff
vmarrerop <vmarrerop@gmail.com>
2024-01-29 18:47:53 -0500

    Add Categoria a post

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    precio: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    unidad: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    cantidad: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    categoria: {  // Agregar la propiedad de categoría
+      type: String,
+      trim: true,
+    },
+    image: {
+      public_id: String,
+      url: String,
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default mongoose.model("Post", postSchema);
