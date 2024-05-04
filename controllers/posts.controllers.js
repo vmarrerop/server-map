56b2449d063ac1de4284333b3f3e989bf016a4f7
vmarrerop <vmarrerop@gmail.com>
2024-05-04 12:59:39 -0500

    dfsdf

--- controllers/posts.controllers.js
+++ controllers/posts.controllers.js
@@ -13,7 +13,7 @@ export const getPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
   try {
-    const { title, description, precio, precioCompra, unidad, cantidad, categoria, proveedor, sede, nombreInsumo, cantidadInsumo } = req.body;
+    const { title, description, precio, precioCompra, unidad, cantidad, categoria, proveedor, sede, insumo } = req.body;
 
     let image = null;
     if (req.files?.image) {
@@ -24,6 +24,14 @@ export const createPost = async (req, res) => {
         public_id: result.public_id,
       };
     }
+
+    // Procesa insumo para asegurar que es un array de objetos
+    const insumosProcesados = Array.isArray(insumo)
+      ? insumo.map(item => ({
+          nombreInsumo: item.nombreInsumo,
+          cantidadInsumo: item.cantidadInsumo || 1, // Valor predeterminado de cantidad
+        }))
+      : []; // Por defecto, insumo es un array vacío si no está definido correctamente
 
     const newPost = new Post({
       title,
@@ -36,8 +44,7 @@ export const createPost = async (req, res) => {
       categoria,
       proveedor,
       sede,
-      nombreInsumo,
-      cantidadInsumo,
+      insumo: insumosProcesados,
     });
 
     await newPost.save();
