0fcf668f16d5ae54ac2a0127f486978267b36f5a
vmarrerop <vmarrerop@gmail.com>
2024-05-28 11:23:01 -0500

    fixxing post

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://jVicente:12345@cluster0.u8fdjxz.mongodb.net/?retryWrites=true&w=majority";
-export const PORT = process.env.PORT || 3001;
+export const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.json());
